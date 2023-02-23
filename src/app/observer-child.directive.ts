78fc4f210ae365ef9c296240f417d4e9577b4a6e
Eric Li <Eric.Li@vista.co>
2023-02-23 23:27:50 +1300

    added intersection observer directive

---
+++ src/app/observer-child.directive.ts
@@ -0,0 +1,45 @@
+import { Directive, Input } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { OnDestroy, AfterViewInit } from '@angular/core';
+
+interface ObserverChildContext {
+  observer: IntersectionObserver;
+}
+
+@Directive({
+  selector: '[arenaOfChoicesObserverChild]',
+  standalone: true,
+})
+export class ObserverChildDirective implements OnDestroy, AfterViewInit {
+  public isIntersecting: boolean;
+
+  // These are just some human-friendly constants to make the HTML template a bit more
+  // readable when being consumed as part of SWTCH/CASE statements.
+  public IS_INTERSECTING = true;
+  public IS_NOT_INTERSECTING = false;
+
+  @Input() observer!: IntersectionObserver | null;
+
+  private elementRef: ElementRef;
+  constructor(elementRef: ElementRef) {
+    this.elementRef = elementRef;
+
+    this.isIntersecting = false;
+  }
+
+  static ngTemplateContextGuard(
+    directive: ObserverChildDirective,
+    context: unknown
+  ): context is ObserverChildContext {
+    return true;
+  }
+
+  public ngAfterViewInit(): void {
+    this.observer?.observe(this.elementRef.nativeElement);
+  }
+
+  public ngOnDestroy(): void {
+    this.observer?.disconnect();
+    this.observer = null;
+  }
+}

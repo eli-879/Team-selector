cc3e3bf54dbf614eda9ad7e71f126a0148922def
Eric Li <Eric.Li@vista.co>
2023-02-26 18:42:25 +1300

    added assets

---
+++ src/app/core/models/asset-container.interface.ts
@@ -0,0 +1,8 @@
+export interface AssetContainer {
+    woodyAssets: HTMLImageElement[];
+    firzenAssets: HTMLImageElement[];
+    henryAssets: HTMLImageElement[];
+    justinAssets: HTMLImageElement[];
+    background: HTMLImageElement | null;
+    damageSplats: HTMLImageElement[];
+}

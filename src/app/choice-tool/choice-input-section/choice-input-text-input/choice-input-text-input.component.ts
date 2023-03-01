b07a9899248cea376850b1ebcfa794056b701244
Eric Li <Eric.Li@vista.co>
2023-03-01 16:19:59 +1300

    button styling

--- src/app/choice-tool/choice-input-section/choice-input-text-input/choice-input-text-input.component.ts
+++ src/app/choice-tool/choice-input-section/choice-input-text-input/choice-input-text-input.component.ts
@@ -2,6 +2,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChoiceToolStore } from '../../store/choice-tool.store';
 import { AssetManagerService } from 'src/app/asset-manager.service';
+import { ColorStore } from 'src/app/color-store/color.store';
+import { Observable } from 'rxjs';
+import { ColorPallete } from 'src/app/color-store/types/color-pallete.interface';
 
 @Component({
     selector: 'arena-of-choices-choice-input-text-input',
@@ -12,10 +15,15 @@ import { AssetManagerService } from 'src/app/asset-manager.service';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChoiceInputTextInputComponent {
+    public colorPallete$: Observable<ColorPallete>;
+
     constructor(
         private choiceToolStore: ChoiceToolStore,
-        private assetManager: AssetManagerService
-    ) {}
+        private assetManager: AssetManagerService,
+        private colorStore: ColorStore
+    ) {
+        this.colorPallete$ = this.colorStore.colorPallete$;
+    }
 
     public addChoice(choice: string) {
         if (!choice.trim()) {
